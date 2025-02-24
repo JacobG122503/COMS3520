663dd2c1f1fb3e7d51503a779f9f06669c48f1cb
jacobg <jaybob1225@gmail.com>
2025-02-24 15:23:55 -0600

    Test function one

--- kernel/proc.c
+++ kernel/proc.c
@@ -681,3 +681,8 @@ procdump(void)
     printf("\n");
   }
 }
+
+//Return parent PID
+uint64 sys_getppid(void) {
+  return myproc()->parent->pid;  
+}
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -89,3 +89,5 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64 sys_getppid(void);
