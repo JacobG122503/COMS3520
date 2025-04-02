e07fa0c8c7ae3460da908a72ac08c852497c7ffe
jacobg <jaybob1225@gmail.com>
2025-04-01 19:03:14 -0500

    test 18

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -103,10 +103,10 @@ extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
 
 //For assignment 1c
-extern uint64 nice(void);
-extern uint64 startcfs(void);
-extern uint64 stopcfs(void);
-extern uint64 getruntime(void);
+extern uint64 nice(int);
+extern void startcfs(int , int , int );
+extern void stopcfs(void);
+extern int getruntime(uint64*, uint64*);
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
