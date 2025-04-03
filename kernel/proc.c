ea2d2610c35c54f832999f9f67aeb652c9831654
jacobg <jaybob1225@gmail.com>
2025-04-03 12:47:12 -0500

    test

--- kernel/proc.c
+++ kernel/proc.c
@@ -532,6 +532,9 @@ void cfs_scheduler(struct cpu *c) {
         inc = (inc < 1) ? 1 : inc;
         cfs_current_proc->vruntime += inc;
         cfs_current_proc->runtime += (cfs_proc_timeslice_len - cfs_proc_timeslice_left);
+
+        printf("[CODE TEST] Process %d Updated: runtime = %d, vruntime = %d\n", 
+          cfs_current_proc->pid, cfs_current_proc->runtime, cfs_current_proc->vruntime);   
         
         // Debug: Process used its timeslice and is swapping out
         printf("[DEBUG CFS] Process %d used %d ticks of its assigned timeslice (totally %d ticks) and swapped out!\n", 
